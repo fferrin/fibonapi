7246311570c569b7d114f45c4b5178d6c604286a
Facundo Ferrin <facundo.ferrin@gmail.com>
2025-05-12 16:58:14 +0200

    Remove initial Fibonacci computation

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,23 +1,6 @@
 import pytest
 
-from fibonacci import fibonacci, FibonacciService
-
-
-@pytest.mark.parametrize(
-    ("n", "result"),
-    (
-        (0, 0),
-        (1, 1),
-        (2, 1),
-        (3, 2),
-        (4, 3),
-        (5, 5),
-        (30, 832_040),
-        (100, 354_224_848_179_261_915_075),
-    ),
-)
-def test_compute_fibonacci(n, result):
-    assert fibonacci(n) == result
+from fibonacci import FibonacciService
 
 
 class TestFibonacciService:
